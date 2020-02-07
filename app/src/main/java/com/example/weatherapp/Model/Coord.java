dab99f75ed226323c4f4214126dd919b4a7c1774
Sally Ho <sally.ho12@yahoo.com>
2020-02-07 15:26:54 -0800

    added getters and setters to get longitude and latitude

--- app/src/main/java/com/example/weatherapp/Model/Coord.java
+++ app/src/main/java/com/example/weatherapp/Model/Coord.java
@@ -1,4 +1,27 @@
 package com.example.weatherapp.Model;
 
 public class Coord {
+    private double myLat;
+    private double myLon;
+
+    public Coord(double theLat, double theLon) {
+        this.myLat = theLat;
+        this.myLon = theLon;
+    }
+
+    public double getMyLat() {
+        return myLat;
+    }
+
+    public void setmyLat(double theLat) {
+        this.myLat = theLat;
+    }
+
+    public double getMyLon() {
+        return myLon;
+    }
+
+    public void setMyLon(double theLon) {
+        this.myLon = theLon;
+    }
 }
